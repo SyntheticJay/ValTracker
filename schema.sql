cfcde77252a1c8d8628a1247c5a757c9f91511cc
Jay <dumpsteredbae@gmail.com>
2022-06-19 21:41:38 +0100

    impl: add SQL table schema's to root

---
+++ schema.sql
@@ -0,0 +1,44 @@
+/*
+ Navicat MariaDB Data Transfer
+
+ Source Server         : ValoTracker
+ Source Server Type    : MariaDB
+ Source Server Version : 100332
+ Source Host           : 93.114.133.122:3306
+ Source Schema         : valotracker
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100332
+ File Encoding         : 65001
+
+ Date: 19/06/2022 21:41:13
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for commands_executed
+-- ----------------------------
+DROP TABLE IF EXISTS `commands_executed`;
+CREATE TABLE `commands_executed`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `server_id` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `command` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `executed_on` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for server_settings
+-- ----------------------------
+DROP TABLE IF EXISTS `server_settings`;
+CREATE TABLE `server_settings`  (
+  `server_id` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `prefix` varchar(3) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `patch_notes` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`server_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
