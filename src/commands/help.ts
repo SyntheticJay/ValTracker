427f382d9b91032ccd56e9f57e3e084041673b22
Jay <dumpsteredbae@gmail.com>
2022-06-19 21:38:42 +0100

    impl: better help command

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -1,34 +1,41 @@
-import { ValorantTracker } from "../index";
-import { ICommand, ICommandContext } from "../types";
-import { CustomEmbed } from "../index";
+import { ValorantTracker } from '../index';
+import { ICommand, ICommandContext } from '../types';
+import { CustomEmbed } from '../index';
 
 export const command: ICommand = {
-  name: "help",
-  description: "Speaks for itself...",
-  usage: "",
-  aliases: ["h"],
-  handler: async (
-    bot: ValorantTracker,
-    context: ICommandContext
-  ): Promise<void> => {
-    const embed = new CustomEmbed().setTitle("Help");
+	name: 'help',
+	description: 'Speaks for itself...',
+	usage: '',
+	aliases: ['h'],
+	handler: async (bot: ValorantTracker, context: ICommandContext): Promise<void> => {
+		const commands = bot
+			.getCommandHandler()
+			.getCommands()
+			.filter((command) => command.name != 'help');
 
-    const commands = bot
-      .getCommandHandler()
-      .getCommands()
-      .filter((command) => command.name != "help");
+		if (context.args.length == 0) {
+			return await bot.getCommandHandler().sendEmbed(context.message, [
+				new CustomEmbed().setTitle('Help').setDescription(`
+				  There are ${commands.length} commands within Valorant Tracker.
+				  Use ${context.config.prefix}help <command> for more information.
+				 
+				  ${commands.map((command) => command.name)}
+      `),
+			]);
+		}
 
-    commands.forEach((command) => {
-      embed.addField(
-        `${context.config.prefix}${command.name}`,
-        `${command.description} | **${
-          command.usage == ""
-            ? command.name
-            : `${context.config.prefix}${command.usage}`
-        }**`
-      );
-    });
+		const command = commands.find((command) => command.name.toLowerCase() == context.args[0].toLowerCase());
 
-    return await bot.getCommandHandler().sendEmbed(context.message, [embed]);
-  },
+		if (!command) {
+			return await bot.getCommandHandler().sendMessage(context.message, 'I could not find that command.', 'FAIL');
+		}
+
+		return await bot.getCommandHandler().sendEmbed(context.message, [
+			new CustomEmbed().setTitle(command.name).setDescription(`
+				  ${command.description}
+				  
+				  Usage: **${command.usage == '' ? 'No arguments required' : command.usage}**
+				`),
+		]);
+	},
 };
