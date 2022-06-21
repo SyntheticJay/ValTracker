f1fdc75f7a09ad8b3042a41e8792c3a068c9283f
Jay <dumpsteredbae@gmail.com>
2022-06-22 00:06:00 +0100

    impl: patchnotes setting

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,87 +1,103 @@
-import { ValorantTracker } from "../index";
-import { ICommand, ICommandContext } from "../types";
-import { CustomEmbed } from "../index";
+import { ValorantTracker } from '../index';
+import { ICommand, ICommandContext } from '../types';
+import { CustomEmbed } from '../index';
+import { Server } from 'guilded.ts';
 
 export const command: ICommand = {
-  name: "settings",
-  description: "Config Valorant Tracker for your server",
-  usage: "settings <config> <value> (leave blank for help)",
-  aliases: ["p"],
-  handler: async (
-    bot: ValorantTracker,
-    context: ICommandContext
-  ): Promise<void> => {
-    const key = context.args[0];
+	name: 'settings',
+	description: 'Config Valorant Tracker for your server',
+	usage: 'settings <config> <value> (leave blank for help)',
+	aliases: ['p'],
+	handler: async (bot: ValorantTracker, context: ICommandContext): Promise<void> => {
+		const key = context.args[0];
 
-    if (!key) {
-      return await bot.getCommandHandler().sendEmbed(context.message, [
-        new CustomEmbed().setTitle("Server Settings").setFields([
-          {
-            name: "Prefix",
-            value: `${context.config.prefix}settings prefix <value>`,
-          },
-          {
-            name: "Patch Notes",
-            value: `${context.config.prefix}settings patchnotes <channelID>`,
-          },
-        ]),
-      ]);
-    }
+		if (!key) {
+			return await bot.getCommandHandler().sendEmbed(context.message, [
+				new CustomEmbed().setTitle('Server Settings').setFields([
+					{
+						name: 'Prefix',
+						value: `${context.config.prefix}settings prefix <value>`,
+					},
+					{
+						name: 'Patch Notes',
+						value: `${context.config.prefix}settings patchnotes <channelID or mention>`,
+					},
+				]),
+			]);
+		}
 
-    const value = context.args[1];
+		let value = context.args[1];
 
-    if (!value) {
-      return await bot
-        .getCommandHandler()
-        .sendMessage(context.message, "Please specify a value!", "FAIL");
-    }
+		if (!value) {
+			return await bot.getCommandHandler().sendMessage(context.message, 'Please specify a value!', 'FAIL');
+		}
 
-    switch (key) {
-      case "prefix":
-        if (value.length > 3 && value != "default") {
-          return await bot
-            .getCommandHandler()
-            .sendMessage(
-              context.message,
-              "Prefix must be less than 3 characters!",
-              "FAIL"
-            );
-        }
+		if (key == 'prefix') {
+			if (value.length > 3 && value != 'default') {
+				return await bot
+					.getCommandHandler()
+					.sendMessage(context.message, 'Prefix must be less than 3 characters!', 'FAIL');
+			}
 
-        context.config.prefix =
-          value == "default" ? bot.getConfig().defaultPrefix : value;
+			context.config.prefix = value == 'default' ? bot.getConfig().defaultPrefix : value;
+			await bot
+				.getDatabaseHandler()
+				.updateServerConfiguration(context.config)
+				.catch(async (err) => {
+					bot
+						.getLogger()
+						.error(new Error(`Error updating ${key} (${value}) for server ID ${context.server.id}: ${err}`));
+					await bot
+						.getCommandHandler()
+						.sendMessage(
+							context.message,
+							'There was an error changing this setting. **this has been logged**.',
+							'FAIL',
+						);
+				})
+				.then(async () => {
+					return await bot
+						.getCommandHandler()
+						.sendMessage(context.message, `Prefix for this server has been changed to ${value}`, 'SUCCESS');
+				});
+		}
 
-        await bot
-          .getDatabaseHandler()
-          .updateServerConfiguration(context.config)
-          .catch(async (err) => {
-            bot
-              .getLogger()
-              .error(
-                new Error(
-                  `Error updating prefix for server ID ${context.server.id}: ${err}`
-                )
-              );
-            await bot
-              .getCommandHandler()
-              .sendMessage(
-                context.message,
-                "There was an error changing your prefix **this has been logged**.",
-                "FAIL"
-              );
-          })
-          .then(async () => {
-            return await bot
-              .getCommandHandler()
-              .sendMessage(
-                context.message,
-                `Prefix for this server has been changed to ${context.config.prefix}`,
-                "SUCCESS"
-              );
-          });
-        break;
-      case "patchnotes":
-        break;
-    }
-  },
+		if (key == 'patchnotes') {
+			if (context.message.mentions?.channels && context.message.mentions.channels.length > 0) {
+				value = context.message.mentions.channels[0].id;
+			}
+
+			bot.channels
+				.fetch(value)
+				.catch(async () => {
+					return await bot.getCommandHandler().sendMessage(context.message, 'Invalid channel ID!', 'FAIL');
+				})
+				.then(async (channel) => {
+					if (channel != undefined) {
+						context.config.patch_notes = channel.id;
+
+						await bot
+							.getDatabaseHandler()
+							.updateServerConfiguration(context.config)
+							.catch(async (err) => {
+								bot
+									.getLogger()
+									.error(new Error(`Error updating ${key} (${value}) for server ID ${context.server.id}: ${err}`));
+								await bot
+									.getCommandHandler()
+									.sendMessage(
+										context.message,
+										'There was an error changing this setting. **this has been logged**.',
+										'FAIL',
+									);
+							})
+							.then(async () => {
+								return await bot
+									.getCommandHandler()
+									.sendMessage(context.message, `Patch Notes for this server has been changed to ${value}`, 'SUCCESS');
+							});
+					}
+				});
+		}
+	},
 };
