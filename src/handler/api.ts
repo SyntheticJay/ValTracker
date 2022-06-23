446c3f97c1e386486734c8f02dc940e940704635
Jay <33841573+SyntheticJay@users.noreply.github.com>
2022-06-23 17:14:55 +0100

    fix: weird indenting issue

--- src/handler/api.ts
+++ src/handler/api.ts
@@ -1,11 +1,11 @@
 import { ValorantTracker } from '../index';
 
 class ValorantAPIHandler {
-	private readonly bot!: ValorantTracker;
+  private readonly bot!: ValorantTracker;
 
-	constructor(bot: ValorantTracker) {
-		this.bot = bot;
-	}
+  constructor(bot: ValorantTracker) {
+     this.bot = bot;
+  }
 }
 
 export { ValorantAPIHandler };
